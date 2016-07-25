db2f99e845a1582622a79f1f553a79f336d2be15
Gerhard Weis <g.weis@griffith.edu.au>
2016-07-26 08:48:38 +1000

    add cov stats to gpp

--- environmental/gpp/convert.py
+++ environmental/gpp/convert.py
@@ -7,6 +7,9 @@ import json
 import tempfile
 import shutil
 import sys
+import numpy as np
+from osgeo import gdal
+from scipy import stats
 import re
 from collections import namedtuple
 
@@ -23,7 +26,7 @@ LAYER_MAP = {
     'gppyrmax_2000_07_molco2yr.tif':    ('GPP1', '2000-2007'),
     'gppyrmean_2000_07_molco2yr.tif':   ('GPP2', '2000-2007'),
     'gppyrmin_2000_07_molco2yr.tif':    ('GPP3', '2000-2007'),
-    
+
     # gpp_year_means2000_2007
     'gppyr_2000_01_molco2m2yr_m.tif':   ('GPP4', '2000'),
     'gppyr_2001_02_molco2m2yr_m.tif':   ('GPP4', '2001'),
@@ -61,7 +64,7 @@ def gen_metadatajson(src, dest):
     md[u'files'] = {}
     for filename in glob.glob(os.path.join(dest, '*', '*.tif')):
         layer_id, time_range = LAYER_MAP[os.path.basename(filename)]
-        md[u'title'] = TITLE_TEMPLATE.format(time_range, FOLDERS[src])   
+        md[u'title'] = TITLE_TEMPLATE.format(time_range, FOLDERS[src])
         filename = filename[len(os.path.dirname(dest)):].lstrip('/')
         md[u'files'][filename] = {
             u'layer': layer_id,
@@ -91,6 +94,72 @@ def zip_dataset(ziproot, dest):
     )
     if ret != 0:
         raise Exception("can't zip {0} ({1})".format(ziproot, ret))
+
+
+def calc_cov(dsfiles):
+    # dsfiles ... list of files to calculate CoV from
+    # returns numpy array
+
+    # open files
+    datasets = [gdal.Open(fname) for fname in dsfiles]
+    # check shape of all datasets:
+    shape = set((ds.RasterYSize, ds.RasterXSize) for ds in datasets)
+    if len(shape) != 1:
+        raise Exception("Raster have different shape")
+    ysize, xsize = shape.pop()
+    result = np.zeros((ysize, xsize), dtype=np.float32)
+    # build buffer array for blocked reading (assume same block size for all datasets, and only one band)
+    x_block_size, y_block_size = datasets[0].GetRasterBand(1).GetBlockSize()
+    #import pdb; pdb.set_trace()
+    for i in range(0, ysize, y_block_size):
+        # determine block height to read
+        if i + y_block_size < ysize:
+            rows = y_block_size
+        else:
+            rows = ysize - i
+        # determine blogk width to read
+        for j in range(0, xsize, x_block_size):
+            if j + x_block_size < xsize:
+                cols = x_block_size
+            else:
+                cols = xsize - j
+        # create buffer array across all datasets
+        inarr = np.zeros((rows, cols, len(datasets)), dtype=np.int16)
+        for idx, ds in enumerate(datasets):
+            inarr[:,:,idx] = ds.GetRasterBand(1).ReadAsArray(xoff=j, yoff=i,
+                                                             win_xsize=cols, win_ysize=rows)
+        # apply func
+        result[i:i+inarr.shape[0], j:j+inarr.shape[1]] = stats.variation(inarr, axis=2)
+
+    return result
+
+
+def write_array_to_raster(outfile, dataset, template):
+    """Write numpy array to raster (geoTIFF format).
+
+    Keyword arguments:
+    outfile -- name of the output file
+    dataset -- numpy array to be written to file
+    template -- path to a gdal dataset to use as template
+
+    Returns: None.
+    """
+    log.info("Writing to {}".format(outfile))
+
+    # open template dataset
+    templateds = gdal.Open(template)
+
+    # create new dataset
+    outdata = templateds.GetDriver().CreateCopy(outfile, templateds, options=("COMPRESS=LZW", "TILED=YES"))
+
+    # write data to file
+    outdata.GetRasterBand(1).WriteArray(dataset)
+
+    # calculate statistics
+    outdata.GetRasterBand(1).ComputeStatistics(False)
+
+    # flush data to disk
+    outdata.FlushCache()
 
 
 def main(argv):
@@ -126,7 +195,19 @@ def main(argv):
             finally:
                 if ziproot:
                     shutil.rmtree(ziproot)
+        # generate cov
+        dsfiles = glob.glob(os.path.join(src, '*.[Rr][Ss][Tt]'))
+        cov = calc_cov(dsfiles)
+        try:
+            destfile = 'gpp_summary_00_07'
+            ziproot = create_target_dir(dest, destfile)
+            write_array_to_raster(os.path.join(ziproot, 'data', destfile + '_cov.tif'), cov, dsfiles[0])
+            gen_metadatajson(srcfolder, ziproot)
+            zip_dataset(ziproot, dest)
+        finally:
+            if ziproot:
+                shutil.rmtree(ziproot)
+
 
 if __name__ == "__main__":
     main(sys.argv)
-
