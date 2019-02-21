a04c605cb9275d838c762bf6c865b52cd214e7a2
Yong Liaw <y.liaw@griffith.edu.au>
2019-02-21 15:09:12 +1000

    Convert Australian soil grid dataset to coverage json format.

--- src/data_conversion/vocabs/__init__.py
+++ src/data_conversion/vocabs/__init__.py
@@ -2,11 +2,12 @@ from .climate import GCMS  # noqa
 from .geotiff import PREDICTORS  # noqa
 
 from .var_defs import (
-    BIOCLIM_VAR_DEFS, WORLDCLIM_VAR_DEFS,
+    BIOCLIM_VAR_DEFS, WORLDCLIM_VAR_DEFS, NSG_VAR_DEFS
 )
 
 
 VAR_DEFS = {
     **BIOCLIM_VAR_DEFS,
-    **WORLDCLIM_VAR_DEFS
+    **WORLDCLIM_VAR_DEFS,
+    **NSG_VAR_DEFS,	
 }
