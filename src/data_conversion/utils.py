06e7f1a9cde0a5cb13bcb13e03bdaa1777eb7023
Gerhard Weis <g.weis@griffith.edu.au>
2019-02-22 15:09:14 +1000

    add open_gdal_dataset to retry failed gdal.Open

--- src/data_conversion/utils.py
+++ src/data_conversion/utils.py
@@ -2,6 +2,8 @@ import glob
 import os
 import os.path
 import shutil
+import gdal
+import time
 
 
 # convert pixel to projection unit
@@ -32,3 +34,28 @@ def move_files(srcdir, destdir):
     destdir = os.path.abspath(destdir)
     for fname in glob.glob(os.path.join(srcdir, '*')):
         shutil.move(fname, os.path.join(destdir, os.path.basename(fname)))
+
+
+def open_gdal_dataset(path):
+    """Open a GDAL dataset.
+
+    Try opening the given path, which may be a gdal url (e.g. /vsizip/...)
+    and in case of errors back off and retry again for up to 5 minutes?
+
+    return an open gdal.Dataset or None
+    """
+    attempts = 5
+    while attempts:
+        # TODO: we could check if the path exists at all in case it is
+        #       a normal file path; othewise we'd need to parse the
+        #       gdal open string to finde the file name and proto
+        ds = gdal.Open(path)
+        if ds is None:
+            # not open ... sleep for a minute
+            # TODO: can we check some sort of error code?
+            attempts -= 1
+            print("Open {} failed. Try again in a minute. ({} attempts left)".format(path, attempts))
+            time.sleep(60)
+        else:
+            return ds
+    return None
