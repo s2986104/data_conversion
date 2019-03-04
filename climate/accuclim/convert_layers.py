c3dba908b126f41a62840eec4326dabc642ad0a7
Yong Liaw <y.liaw@griffith.edu.au>
2019-03-05 09:59:39 +1000

    Convert layer and generate metadata for ACCUClim.

---
+++ climate/accuclim/convert_layers.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+from concurrent import futures
+import glob
+import os
+import os.path
+import tempfile
+import zipfile
+import argparse
+import shutil
+
+from osgeo import gdal
+import tqdm
+
+from data_conversion.vocabs import VAR_DEFS, PREDICTORS
+from data_conversion.utils import ensure_directory, move_files, retry_run_cmd
+
+
+def gdal_options(srcfile, year):
+    """
+    options to add metadata for the tiff file
+    """
+
+    options = ['-of', 'GTiff', '-co', 'TILED=YES']
+    options += ['-co', 'COMPRESS=DEFLATE', '-norat']
+    options += ['-mo', 'year_range={}-{}'.format(year-14, year+15)]
+    options += ['-mo', 'year={}'.format(year)]
+    return options
+
+
+def get_layer_id(filename):
+    fname = os.path.splitext(os.path.basename(filename))[0]
+    _, layerid, year = fname.split('_')
+    return 'bioclim-{}'.format(layerid), int(year)
+
+
+def run_gdal(cmd, infile, outfile, layerid):
+    _, tfname = tempfile.mkstemp(suffix='.tif')
+    try:
+        retry_run_cmd(cmd + [infile, tfname])
+        # add band metadata
+        # this is our temporary geo tiff, we should be able to open that
+        # without problems
+        ds = gdal.Open(tfname, gdal.GA_Update)
+        band = ds.GetRasterBand(1)
+        # ensure band stats
+        band.ComputeStatistics(False)
+        for key, value in VAR_DEFS[layerid].items():
+            band.SetMetadataItem(key, value)
+        # just for completeness
+        band.SetUnitType(VAR_DEFS[layerid]['units'])
+        # band.SetScale(1.0)
+        # band.SetOffset(0.0)
+        ds.FlushCache()
+        # build command
+        cmd = [
+            'gdal_translate',
+            '-of', 'GTiff',
+            '-co', 'TILED=yes',
+            '-co', 'COPY_SRC_OVERVIEWS=YES',
+            '-co', 'COMPRESS=DEFLATE',
+            '-co', 'PREDICTOR={}'.format(PREDICTORS[band.DataType]),
+        ]
+        # check rs
+        if not ds.GetProjection():
+            cmd.extend(['-a_srs', 'EPSG:4326'])
+        # close dataset
+        del band
+        del ds
+        # gdal_translate once more to cloud optimise geotiff
+        cmd.extend([tfname, outfile])
+        retry_run_cmd(cmd)
+    except Exception as e:
+        print('Error:', e)
+        raise e
+    finally:
+        os.remove(tfname)
+
+
+def convert(srcfile, destdir):
+    """convert .asc.gz files in folder to .tif in dest
+    """
+
+    pool = futures.ProcessPoolExecutor()
+    results = []
+    with zipfile.ZipFile(srcfile) as srczip:
+        for zipinfo in tqdm.tqdm(srczip.filelist, desc="build jobs"):
+            if zipinfo.is_dir():
+                # skip dir entries
+                continue
+
+            if not zipinfo.filename.endswith('tif'):
+                continue
+
+            layerid, year = get_layer_id(os.path.basename(zipinfo.filename))
+            destfilename = 'accuclim_{}_{}.tif'.format(layerid, year) 
+            srcurl = '/vsizip/' + srcfile + '/' + zipinfo.filename
+            gdaloptions = gdal_options(srcfile, year)
+            # output file name
+            destpath = os.path.join(destdir, destfilename)
+            # run gdal translate
+            cmd = ['gdal_translate']
+            cmd.extend(gdaloptions)
+            results.append(
+                pool.submit(run_gdal, cmd, srcurl, destpath, layerid)
+            )
+
+    for result in tqdm.tqdm(futures.as_completed(results),
+                            desc=os.path.basename(srcfile),
+                            total=len(results)):
+        if result.exception():
+            print("Job failed")
+            raise result.excption()
+
+
+def create_target_dir(destdir, srcfile):
+    """create zip folder structure in tmp location.
+    return root folder
+    """
+    dirname = 'accuClim'
+    root = os.path.join(destdir, dirname)
+    os.makedirs(root, exist_ok=True)
+    return root
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'source', action='store',
+        help='source folder or source zip file.'
+    )
+    parser.add_argument(
+        'destdir', action='store',
+        help='destination folder for converted tif files.'
+    )
+    parser.add_argument(
+        '--workdir', action='store',
+        default='/mnt/workdir/australia-5km_work',
+        help=('folder to store working files before moving to final '
+              'destination')
+    )
+    return parser.parse_args()
+
+
+def main():
+    opts = parse_args()
+    srcfile = os.path.abspath(opts.source)
+    if os.path.isdir(srcfile):
+        srcfiles = sorted(glob.glob(os.path.join(srcfile, '*.zip')))
+    else:
+        srcfiles = [srcfile]
+
+    workdir = ensure_directory(opts.workdir)
+    dest = ensure_directory(opts.destdir)
+    # unpack contains one destination datasets
+    for srcfile in tqdm.tqdm(srcfiles):
+        target_work_dir = create_target_dir(workdir, srcfile)
+        try:
+            # convert files into workdir
+            convert(srcfile, target_work_dir)
+            # move results to dest
+            target_dir = create_target_dir(dest, srcfile)
+            move_files(target_work_dir, target_dir)
+        finally:
+            # cleanup
+            shutil.rmtree(target_work_dir)
+
+
+if __name__ == "__main__":
+    main()
---
+++ climate/accuclim/generate_layer_metadata.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python
+import argparse
+import copy
+import glob
+import itertools
+import json
+import os
+import os.path
+import re
+
+import tqdm
+
+from data_conversion.coverage import (
+    gen_tif_metadata,
+    gen_tif_coverage,
+    get_coverage_extent,
+    gen_coverage_uuid,
+    gen_dataset_coverage,
+)
+
+# TODO: need to add resolution to data.json metadata
+#       or just to dataset metadata ... probably more appropriate
+
+# TODO: add mimetype somehwere?
+
+CATEGORY = 'climate'
+RESOLUTION = '9 arcsec'
+SWIFT_CONTAINER = (
+    'https://swift.rc.nectar.org.au/v1/AUTH_0bc40c2c2ff94a0b9404e6f960ae5677/'
+    'accuclim'
+)
+
+RESOLUTIONS = {  # udunits arc_minute / arcmin, UCUM/UOM: min_arc
+    '9 arcsec': '9 arcsec (~250m)',
+}
+
+ACKNOWLEDGEMENT = (
+            'Storlie, C.J., Phillips, B.L., VanDerWal, J.J., and Williams, S.E. (2013) '
+            'Improved spatial estimates of climate predict patchier species distributions. '
+            'Diversity and Distributions, 19 (9). pp. 1106-1113.'
+)
+
+DATASETS = [
+    # current
+    {
+        # bio
+        'title': 'accuCLIM (Wet Tropics Australia), 30-year average either side of ({year}), {resolution}',
+        'acknowledgement': ACKNOWLEDGEMENT,
+        'external_url': 'https://researchdata.ands.org.au/accuclim-30-year-heritage-area/654267',
+        'license': (
+            'Creative Commons Attribution 3.0 AU '
+            'http://creativecommons.org/licenses/by/3.0/au'
+        ),
+        'filter': {
+            'genre': 'DataGenreCC',
+            'year': None
+        },
+        'aggs': [], 
+    }
+]
+
+COLLECTION = {
+    "_type": "Collection",
+    "uuid": "1db9e574-2f14-11e9-b0ea-0242ac110002",
+    "title": "ACCUclim climate data",
+    "description": "A set of 7 temperature variables for the Wet Tropics area in north-east Australia\n\nGeographic extent: Wet Tropics, Australia\nYear range: 1950-2015\nResolution: 9 arcsec (~250 m)\nData layers: B01-07",
+    "rights": "CC-BY Attribution 3.0",
+    "landingPage": "https://researchdata.ands.org.au/accuclim-30-year-heritage-area/654267",
+    "attribution": ["Storlie, C.J., Phillips, B.L., VanDerWal, J.J., and Williams, S.E. (2013) Improved spatial estimates of climate predict patchier species distributions. Diversity and Distributions, 19 (9). pp. 1106-1113."],
+    "subjects": ["Current datasets"],
+    "categories": ["climate"],
+    "datasets": [
+        # {
+        #     "uuid": "8f6e3ea5-1caf-5562-a580-aa23bbe7c975",
+        #     "title": "Australia, Current Climate (1976-2005), 2.5 arcmin (~5 km)"
+        # },
+        # {
+        #     "uuid": "fecc0b23-4199-5b49-ac6c-45c3c1249f3e",
+        #     "title": "Australia, Climate Projection, 2.5 arcmin"
+        # }
+    ],
+
+    "BCCDataGenre": ["DataGenreCC"]
+}
+
+
+def gen_dataset_metadata(dsdef, coverages, **kw):
+    ds_md = {
+        'category': CATEGORY,
+        'genre': kw['genre'],
+        'resolution': RESOLUTION,
+        'acknowledgement': dsdef.get('acknowledgment'),
+        'external_url': dsdef.get('external_url'),
+        'license': dsdef.get('license'),
+        # TODO: format title
+        'title': dsdef['title'].format(resolution=RESOLUTIONS[RESOLUTION], **kw)
+    }
+    # collect some bits of metadata from data
+    if dsdef['filter']['genre'] == 'DataGenreCC':
+        # all coverages have the same year and year_range
+        ds_md['year'] = coverages[0]['bccvl:metadata']['year']
+        ds_md['year_range'] = coverages[0]['bccvl:metadata']['year_range']
+    return ds_md
+
+
+# TODO: duplicate in worldclim/generate_metadata_layers
+def match_coverage(cov, attrs):
+    # used to filter set of coverages
+    md = cov['bccvl:metadata']
+    for attr, value in attrs.items():
+        if isinstance(value, re.Pattern):
+            if not value.match(md[attr]):
+                return False
+            continue
+        if value is None:
+            # attr should not be there
+            if attr in md:
+                return False
+            continue
+        if value == '*':
+            # attr should be there
+            if attr not in md:
+                return False
+            continue
+        if md.get(attr) != attrs[attr]:
+            return False
+    return True
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--force', action='store_true',
+                        help='Re generate data.json form tif files.')
+    parser.add_argument('srcdir')
+    return parser.parse_args()
+
+
+def main():
+    # TODO: we need a mode to just update as existing json file without parsing
+    #       all tiff files. This would be useful to just update titles and
+    #       things.
+    #       could probably also be done in a separate one off script?
+    opts = parse_args()
+    opts.srcdir = os.path.abspath(opts.srcdir)
+
+    datajson = os.path.join(opts.srcdir, 'data.json')
+    print("Generate data.json")
+    if not os.path.exists(datajson) or opts.force:
+        print("Rebuild data.json")
+        # rebuild data.json
+        coverages = []
+        # generate all coverages inside source folder
+        tiffiles = sorted(glob.glob(os.path.join(opts.srcdir, '**/*.tif'),
+                                    recursive=True))
+        for tiffile in tqdm.tqdm(tiffiles):
+            try:
+                md = gen_tif_metadata(tiffile, opts.srcdir, SWIFT_CONTAINER)
+                coverage = gen_tif_coverage(tiffile, md['url'])
+                md['extent_wgs84'] = get_coverage_extent(coverage)
+                md['resolution'] = RESOLUTION
+                if md['genre'] == 'DataGenreCC':
+                    md['acknowledgement'] = ACKNOWLEDGEMENT
+                coverage['bccvl:metadata'] = md
+                coverage['bccvl:metadata']['uuid'] = gen_coverage_uuid(coverage, 'accuclim')
+                coverages.append(coverage)
+            except Exception as e:
+                print('Failed to generate metadata for:', tiffile, e)
+                raise
+
+        print("Write data.json")
+        with open(datajson, 'w') as mdfile:
+            json.dump(coverages, mdfile, indent=2)
+    else:
+        print("Use existing data.json")
+        coverages = json.load(open(datajson))
+
+    print("Generate datasets.json")
+    datasets = []
+    # collect all years from coverages
+    YEARS = sorted({cov['bccvl:metadata']['year'] for cov in coverages if 'year' in cov['bccvl:metadata']})
+    # generate datasets for db import
+    for dsdef in DATASETS:
+        # make a copy so that we can modify the filters
+        dsdef = copy.deepcopy(dsdef)
+        cov_filter = dsdef['filter']
+        if cov_filter['genre'] == 'DataGenreCC':
+            # current
+            for year in YEARS:
+                cov_filter.update({
+                    'year': year
+                })
+                subset = list(filter(
+                    lambda x: match_coverage(x, cov_filter),
+                    coverages
+                ))
+                if not subset:
+                    print("No Data matched for {}".format(cov_filter))
+                    continue
+                coverage = gen_dataset_coverage(subset, dsdef['aggs'])
+                md = gen_dataset_metadata(dsdef, subset, genre=cov_filter['genre'])
+                md['extent_wgs84'] = get_coverage_extent(coverage)
+                coverage['bccvl:metadata'] = md
+                coverage['bccvl:metadata']['uuid'] = gen_coverage_uuid(coverage, 'accuclim')
+                datasets.append(coverage)
+
+    print("Write datasets.json")
+    # save all the data
+    with open(os.path.join(opts.srcdir, 'datasets.json'), 'w') as mdfile:
+        json.dump(datasets, mdfile, indent=2)
+
+    print("Write collection.json")
+    with open(os.path.join(opts.srcdir, 'collection.json'), 'w') as mdfile:
+        # add datasets
+        for ds in datasets:
+            COLLECTION['datasets'].append({
+                "uuid": ds['bccvl:metadata']['uuid'],
+                "title": ds['bccvl:metadata']['title']
+            })
+        json.dump([COLLECTION], mdfile, indent=2)
+
+
+if __name__ == "__main__":
+    main()
