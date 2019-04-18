d39c8f3641186eaf34c9b9a3048073380de6721c
Yong Liaw <y.liaw@griffith.edu.au>
2019-04-18 16:02:47 +1000

    convert layers and generate metadata for AWAP datasets.

--- climate/australian-water-availability-project/convert_layers.py
+++ climate/australian-water-availability-project/convert_layers.py
@@ -1,193 +1,84 @@
 #!/usr/bin/env python
-import os
 import os.path
-import zipfile
-import glob
-import tempfile
-import shutil
-import sys
-import re
-import time
-import argparse
 from datetime import datetime
 
-LAYER_MD = {
-    'mth_FWDis': ('mth_FWDis', 'monthly local discharge (tunoff+drainage) (average)', 'mm/d', None),
-    'pcr_mth_FWDis': ('pcr_mth_FWDis', 'monthly local discharge (runoff+drainage) (percentile rank data)', '%', None),
-    'ann_FWDis': ('ann_FWDis', 'annual local discharge (runoff+drainage) (average)', 'mm/d', None),
-    'pcr_ann_FWDis': ('pcr_ann_FWDis', 'annual local discharge (runoff+drainage) (percentile rank data)', '%', None),
-    'ann_FWE': ('ann_FWE', 'annual total evaporation (soil+vegetation) (average)', 'mm/d', None),
-    'pcr_ann_FWE': ('pcr_ann_FWE', 'annual total evaporation (soil+vegetation) (percentile rank data)', '%', None),
-    'ann_FWLch2': ('ann_FWLch2', 'annual deep drainage (average)', 'mm/d', None),
-    'pcr_ann_FWLch2': ('pcr_ann_FWLch2', 'annual deep drainage (percentile rank data)', '%', None),
-    'ann_FWPT': ('ann_FWPT', 'annual potential evaporation (average)', 'mm/d', None),
-    'pcr_ann_FWPT': ('pcr_ann_FWPT', 'annual potential evaporation (percentile rank data)', '%', None),
-    'ann_FWRun': ('ann_FWRun', 'annual surface runoff (average)', 'mm/d', None),
-    'pcr_ann_FWRun': ('pcr_ann_FWRun', 'annual surface runoff (percentile rank data)', '%', None),
-    'ann_FWSoil': ('ann_FWSoil', 'annual soil evaporation (average)', 'mm/d', None),
-    'pcr_ann_FWSoil': ('pcr_ann_FWSoil', 'annual soil evaporation (percentile rank data)', '%', None),
-    'ann_FWTra': ('ann_FWTra', 'annual total transpiration (average)', 'mm/d', None),
-    'pcr_ann_FWTra': ('pcr_ann_FWTra', 'annual total transpiration (percentile rank data)', '%', None),
-    'ann_FWWater': ('ann_FWWater', 'annual open water evaporation (average)', 'mm/d', None),
-    'pcr_ann_FWWater': ('pcr_ann_FWWater', 'annual open water evaporation (percentile rank data)', '%', None),
-    'ann_PhiE': ('ann_PhiE', 'annual daily latent heat flux (average)', 'W/m^2', None),
-    'pcr_ann_PhiE': ('pcr_ann_PhiE', 'annual daily latent heat flux (percentile rank data)', '%', None),
-    'ann_PhiH': ('ann_PhiH', 'annual daily sensible heat flux (average)', 'W/m^2', None),
-    'pcr_ann_PhiH': ('pcr_ann_PhiH', 'annual daily sensible heat flux (percentile rank data)', '%', None),
-    'ann_SolarMJ': ('ann_SolarMJ', 'annual incident solar radiation (average)', 'MJ/m^2/d', None),
-    'pcr_ann_SolarMJ': ('pcr_ann_SolarMJ', 'annual incident solar radiation (percentile rank data)', '%', None),
-    'ann_TempMax': ('ann_TempMax', 'annual daily maximum temperature (average)', 'degree_Celsius', None),
-    'pcr_ann_TempMax': ('pcr_ann_TempMax', 'annual daily maximum temperature (percentile rank data)', '%', None),
-    'ann_TempMin': ('ann_TempMin', 'annual daily minimum temperature (average)', 'degree_Celsius', None),
-    'pcr_ann_TempMin': ('pcr_ann_TempMin', 'annual daily minimum temperature (percentile rank data)', '%', None),
-    'ann_WRel1': ('ann_WRel1', 'annual relative soil moisture (upper layer)', None, None),
-    'pcr_ann_WRel1': ('pcr_ann_WRel1', 'annual relative soil moisture (upper layer) (percentile rank data)', '%', None),
-    'ann_WRel1End': ('ann_WRel1End', 'annual relative soil moisture (upper layer) at end of aggregation period', None, None),
-    'pcr_ann_WRel1End': ('pcr_ann_WRel1End', 'annual relative soil moisture (upper layer) at end of aggregation period (percentile rank data)', '%', None),
-    'ann_WRel2': ('ann_WRel2', 'annual relative soil moisture (lower layer)', None, None),
-    'pcr_ann_WRel2': ('pcr_ann_WRel2', 'annual relative soil moisture (lower layer) (percentile rank data)', '%', None),
-    'ann_WRel2End': ('ann_WRel2End', 'annual relative soil moisture (lower layer) at end of aggregation period', None, None),
-    'pcr_ann_WRel2End': ('pcr_ann_WRel2End', 'annual relative soil moisture (lower layer) at end of aggregation period (percentile rank data)', '%', None),
-    'ann_Precip': ('ann_Precip', 'annual precipitation (average)', 'mm/d', None),
-    'pcr_ann_Precip': ('pcr_ann_Precip', 'annual relative precipitation (percentile rank data)', '%', None)
-}
+from data_conversion.converter import BaseConverter
 
 
-def unpack(zipname, path):
-    """unpack zipfile to path
-    """
-    tries = 0
-    while True:
-        try:
-            tries += 1
-            zipf = zipfile.ZipFile(zipname, 'r')
-            zipf.extractall(path)
-            print "File {0} is online".format(zipname)
-            break
-        except Exception as e:
-            if tries > 10:
-                print "Fail to make file {0} online!!".format(zipname)
-                raise Exception("Error: File {0} is not online".format(zipname))
-            print "Waiting for file {0} to be online ...".format(zipname)
-            time.sleep(60)
+class AwapConverter(BaseConverter):
+
+    def parse_zip_filename(self, srcfile):
+        """
+        Parse filename of the format 19870101_19871231.FWDis.run26j.flt.zip 
+        to get layerid
+        """
+        # this should always parse source file ... we know the dest file anyway
+        basename = os.path.basename(srcfile)
+        parts = os.path.splitext(basename)[0].split('.')
+
+        date = parts[0].split('_')[0]
+        year = datetime.strptime(date, '%Y%m%d').year
+        return {
+            'year': year,
+            'run': parts[2]
+        }
+
+    def parse_filename(self, filename):
+        # AWAP/Run26h/FWDis/pcr_ann_FWDis_20051231.flt
+        parts = os.path.splitext(os.path.basename(filename))[0].split('_')
+        date = parts[-1]
+        year = datetime.strptime(date, '%Y%m%d').year
+        layerid = '_'.join(parts[:-1])
+
+        return {
+            'layerid': layerid,
+            'year': year
+        }
+
+    def filter_srcfiles(self, srcfile):
+        """
+        return False to skip this srcfile (zip file)
+        """
+        # only process zip files for Run26j
+        return 'run26j' in os.path.basename(srcfile).split('.')
+
+    def skip_zipinfo(self, zipinfo):
+        """
+        return true to ignore this zipinfo entry
+        """
+        # default ignore directories
+        if zipinfo.is_dir():
+            return True
+        # ignore none .tif, .asc files
+        _, ext = os.path.splitext(zipinfo.filename)
+        if ext not in ('.flt',):
+            return True
+
+        # only want annual layer
+        md = self.parse_filename(zipinfo.filename)
+        return 'ann' not in md['layerid'].split('_')
+
+    def gdal_options(self, md):
+        """
+        options to add metadata for the tiff file
+        """
+        year = md['year']
+        options = ['-of', 'GTiff', '-co', 'TILED=YES']
+        options += ['-co', 'COMPRESS=DEFLATE', '-norat']
+        options += ['-mo', 'year_range={}-{}'.format(year, year)]
+        options += ['-mo', 'year={}'.format(year)]
+        return options
+
+    def target_dir(self, destdir, srcfile):
+        md = self.parse_zip_filename(srcfile)
+        dirname = 'awap_{}_{}'.format(md['run'], md['year'])
+        return os.path.join(destdir, dirname)
 
 
-def get_md(filename):
-    # Get the layer md
-    nameparts = os.path.splitext(filename)[0].split('_')
-    date = nameparts[-1]
-    year = datetime.strptime(date, '%Y%m%d').year
-    layer_name = '_'.join(nameparts[:-1])
-    md = LAYER_MD.get(layer_name)
-    return md, year
+def main():
+    converter = AwapConverter()
+    converter.main()
 
-
-def metadata_options(md, year):
-    # options to add metadata for the tiff file
-    options = '-of GTiff -co "COMPRESS=LZW" -co "TILED=YES" -a_srs EPSG:4326'
-
-    if year:
-        options += ' -mo "year={}"'.format(year)
-    if md[0]:
-        options += ' -mo "standard_name={}"'.format(md[0])
-    if md[1]:
-        options += ' -mo "long_name={}"'.format(md[1])
-    if md[2]:
-        options += ' -mo "unit={}"'.format(md[2])
-    return options
-
-def convert(folder, dest, only_year=None):
-    """convert .flt files in folder to .tif in dest
-    """
-    # only interested in annual data
-    glob_filename = '*/*/*/*ann*.flt' 
-    if only_year is not None:
-        glob_filename = '*/*/*/*ann*{}*.flt'.format(only_year)
-
-    for srcfile in glob.glob(os.path.join(folder, glob_filename)):
-        filename = os.path.basename(srcfile)[:-len('.flt')] + '.tif'
-        md, year = get_md(filename)
-        if md is None:
-            continue
-        options = metadata_options(md, year)   
-        # dest filename = dirname_variablename.tif
-        dfilename = os.path.basename(dest) + '_' + filename
-        destfile = os.path.join(dest, dfilename) 
-        ret = os.system('gdal_translate {2} {0} {1}'.format(srcfile, 
-                                                            destfile, 
-                                                            options))
-        if ret != 0:
-            raise Exception("can't gdal_translate {0} ({1})".format(srcfile,
-                                                                    ret))
-        # Add factor and offset metedata to the band data
-        scale = md[3]
-        if scale is not None:
-            ret = os.system('gdal_edit.py -scale {0} -offset {1} {2}'.format(scale, 0, destfile))
-            if ret != 0:
-                raise Exception("can't gdal_edit.py {0} ({1})".format(destfile, scale))
-
-
-def unzip_dataset(dsfile):
-    """unzip source dataset and return unzip location
-    """
-    tmpdir = tempfile.mkdtemp()
-    try:
-        unpack(dsfile, tmpdir)
-    except:
-        shutil.rmtree(tmpdir)
-        raise
-    return tmpdir
-
-
-def create_target_dir(destdir, srcfile):
-    """create zip folder structure in tmp location.
-    return root folder
-    """
-    # Extract the date from source file name i.e. 19500101_19501231.FWDis.run26j.flt.zip
-    filename = os.path.basename(srcfile)
-    date = filename.split('.')[0].split('_')[1]
-    root = os.path.join(destdir, 'awap_ann_{}'.format(date))
-    if not os.path.isdir(root):
-        os.mkdir(root)
-    return root
-
-
-def main(argv):
-    LAYER_TYPES = ['FWDis', 'FWE', 'FWLch2', 'FWPT', 'FWRun', 'FWSoil', 'FWTra', 'FWWater', 'PhiE', 'PhiH', 'Precip', 'SolarMJ', 'TempMax', 'TempMin', 'WRel1', 'WRel1End', 'WRel2', 'WRel2End']
-    parser = argparse.ArgumentParser(description='Convert AWAP layers')
-    parser.add_argument('srcdir', type=str, help='source directory')
-    parser.add_argument('destdir', type=str, help='output directory')
-    parser.add_argument('--dstype', type=str, choices=LAYER_TYPES, help='layer type')
-    parser.add_argument('--year', type=int, help='year')
-    params = vars(parser.parse_args(argv[1:]))
-    import pdb; pdb.set_trace()
-    srcdir = params.get('srcdir')
-    destdir = params.get('destdir')
-    year = params.get('year')
-    dstypes = LAYER_TYPES if params.get('dstype') is None else [params.get('dstype')]
-
-    ziproot = None
-    srctmpdir = None
-
-    for dstype in LAYER_TYPES:
-        if dstype not in dstypes:
-            continue
-        for srcfile in glob.glob(os.path.join(srcdir, dstype, '*.zip')):
-            try:
-                # TODO: check src exists and is zip?
-                # TODO: check destdir exists
-                srctmpdir = unzip_dataset(srcfile)
-                # unpack contains one destination datasets
-                ziproot = create_target_dir(destdir, srcfile)
-                convert(srctmpdir, ziproot, year)
-                if srctmpdir:
-                    shutil.rmtree(srctmpdir)
-            except Exception as e:
-                # cleanup temp location
-                if srctmpdir:
-                    shutil.rmtree(srctmpdir)
-
-                print("Error: Cannot convert {}".format(srcfile))
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main()
---
+++ climate/australian-water-availability-project/generate_layer_metadata.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+import os.path
+
+from data_conversion.converter import BaseLayerMetadata
+from data_conversion.coverage import gen_coverage_uuid
+from data_conversion.vocabs import RESOLUTIONS
+
+
+class AwapLayerMetadata(BaseLayerMetadata):
+
+    # all datasets are of 'hydrology sciemtific type'
+    CATEGORY = 'hydrology'
+    # TODO: should we rather set the id in DATASETS list?
+    #       category as well?
+    DATASET_ID = 'awap'
+    # swift base url for this data
+    SWIFT_CONTAINER = (
+        'https://swift.rc.nectar.org.au/v1/AUTH_0bc40c2c2ff94a0b9404e6f960ae5677/'
+        'awap_layers'
+    )
+
+    DATASETS = [
+        # current
+        {
+            # bio
+            'title': 'Australia, Water Availability ({year}), {resolution}',
+            'acknowledgement': (
+                'Raupach MR, PR Briggs, V Haverd, EA King, M Paget, CM Trudinger (2009), '
+                'Australian Water Availability Project (AWAP): CSIRO Marine and '
+                'Atmospheric Research Component: Final Report for Phase 3. CAWCR '
+                'Technical Report No. 013. 67 pp.'
+            ),
+            'filter': {
+                'genre': 'DataGenreE',
+                'year': None
+            },
+            'aggs': [],
+        }
+    ]
+
+    COLLECTION = {
+        "_type": "Collection",
+        "uuid": "88b335a1-ce16-46a2-aa7c-5b1b8049ecd4",
+        "title": "Australian Water Availability Project",
+        "description": (
+            "Annual data about the state and trend of the terrestrial water balance in Australia.\n\n"
+            "Geographic extent: Australia\nYear range: 1900-2013\n"
+            "Resolution: 3 arcmin (~5 km)\nData layers: 36 layers including runoff, evaporation, soil moisture and heat flux"
+        ),
+        "rights": "CC-BY Attribution 3.0",
+        "landingPage": "See <a href=\"http://www.csiro.au/awap/\">http://www.csiro.au/awap/</a>",
+        "attribution": ["Raupach MR, PR Briggs, V Haverd, EA King, M Paget, CM Trudinger (2009), Australian Water Availability Project (AWAP): CSIRO Marine and Atmospheric Research Component: Final Report for Phase 3. CAWCR Technical Report No. 013. 67 pp."],
+        "subjects": ["Current datasets"],
+        "categories": ["environmental"],
+        "BCCDataGenre": ["DataGenreE"],
+        # will be created/filled by metadata generator
+        "datasets": [],
+    }
+
+    def parse_filename(self, tiffile):
+        return {
+            'resolution': RESOLUTIONS['180']['long'],
+        }
+
+    def gen_dataset_metadata(self, dsdef, coverages):
+        ds_md = {
+            # apply filter values as metadata
+            # apply metadata bits from dsdef
+            'category': self.CATEGORY,
+            'genre': dsdef['filter']['genre'],
+            'resolution': RESOLUTIONS['180']['long'],
+            'acknowledgement': dsdef.get('acknowledgment'),
+            'external_url': dsdef.get('external_url'),
+            'license': dsdef.get('license'),
+            'title': dsdef['title'].format(resolution=RESOLUTIONS['180']['long'], **dsdef['filter'])
+        }
+        # collect some bits of metadata from data
+        # all coverages have the same year and year_range
+        ds_md['year'] = coverages[0]['bccvl:metadata']['year']
+        ds_md['year_range'] = coverages[0]['bccvl:metadata']['year_range']ß
+        return ds_md
+
+    def get_genre(self, md):
+        return 'DataGenreE'
+
+
+def main():
+    gen = AwapLayerMetadata()
+    gen.main()
+
+if __name__ == "__main__":
+    main()
