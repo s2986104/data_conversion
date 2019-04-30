e6d82c0973dc3fd855b66b6285dd9f77e36e6564
Gerhard Weis <g.weis@griffith.edu.au>
2019-04-30 11:19:23 +1000

    closing quote

--- environmental/geofabric/convert_all.sh
+++ environmental/geofabric/convert_all.sh
@@ -3,7 +3,7 @@
 # This is for collections for geofabric data, which is handled differently from other datasets.
 # Run this to generate the data layers for Geofabric data 
 
-BASEDIR="/mnt/collection/datasets/environmental/geofabric
+BASEDIR="/mnt/collection/datasets/environmental/geofabric"
 
 python3 convert_layers.py ${BASEDIR}/source/NationalCatchmentBoundariesRaster1.tif ${BASEDIR}/layers
 python3 convert_layers.py ${BASEDIR}/source/DEMDerivedStreamsRaster1.tif ${BASEDIR}/layers
