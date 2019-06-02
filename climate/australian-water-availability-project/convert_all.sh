e655674e770e54d1af4a48e4fc53616b1f367e87
Gerhard Weis <g.weis@griffith.edu.au>
2019-06-03 09:20:19 +1000

    clean up awap

---
+++ climate/australian-water-availability-project/convert_all.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# python3 convert_layers.py ./source /mnt/workdir/australia-5km/bccvl/layers
+# python3 convert_layers.py ./source bccvl/layers
+
+BASEDIR="/mnt/collection/datasets/climate/australian-water-availability-project"
+
+python3 convert_layers.py ${BASEDIR}/source ${BASEDIR}/layers
+
+# generate layer metadata
+python3 generate_layer_metadata.py --force ${BASEDIR}/layers
--- climate/australian-water-availability-project/convert_all_layers.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in source/*.zip ; do
-    echo "convert $file"
-    python convert_layers.py $file bccvl/layers
-done
