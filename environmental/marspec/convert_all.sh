c7f15fbf32cc710c3927e8e0cb11ba0355c2954b
Gerhard Weis <g.weis@griffith.edu.au>
2019-06-03 10:17:55 +1000

    add marspec convert shell script

---
+++ environmental/marspec/convert_all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This is for Australian Topography collection
+# Run this to for multi-resolution-valley-bottom-flatness
+# and multi-resolution-ridge-top-flatness datasets
+
+
+BASEDIR="/mnt/collection/datasets/environmental/marspec"
+
+python3 convert_layers.py ${BASEDIR}/source ${BASEDIR}/layers
+
+# generate layer metadata
+python3 generate_layer_metadata.py --force ${BASEDIR}/layers 
